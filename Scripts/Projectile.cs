6103783823d1875e2cbdf5bed281761fb7735a1a
7Ratchet <109304910+7Ratchet@users.noreply.github.com>
2025-07-31 17:35:56 +0200

    Added ttl to bullets

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -3,9 +3,16 @@ using System;
 
 public partial class Projectile : Area2D
 {
-    [Export] public float speed = 20;
+    [Export] public float speed = 8;
+    [Export] public float ttl = 5;
+
+    private double time = 0;
 
     public override void _PhysicsProcess(double delta) {
+        time += delta;
         Position += new Vector2(0, -speed).Rotated(Rotation);
+        if (ttl < time) {
+            QueueFree();
+        }
     }
 }
