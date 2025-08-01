de58e7296f61adbbef849abd36585e2b5283206f
7Ratchet <109304910+7Ratchet@users.noreply.github.com>
2025-08-01 15:43:38 +0200

    Enemy goes back to his place where he belongs when timer is up

--- Scripts/EnemyType.cs
+++ Scripts/EnemyType.cs
@@ -2,27 +2,38 @@ using Godot;
 using System;
 
 public partial class EnemyType : RigidBody2D {
-
     private RayCast2D detectCast;
-
     private Vector2 goTo;
+    private Vector2 ogPos;
+    private double timer = 0;
+    [Export] private float timeToGoBack = 5;
+    [Export] private float speed = 0.01f;
 
     public override void _Ready() {
         detectCast = GetNode<RayCast2D>("DetectCast");
         goTo = GlobalPosition;
+        ogPos = GlobalPosition;
     }
 
     public override void _PhysicsProcess(double delta) {
-        findPlayer();
+        findPlayer(delta);
     }
 
-    private void findPlayer() {
+    private void findPlayer(double delta) {
         detectCast.TargetPosition = ToLocal(Global.Instance.player.GlobalPosition);
         if (detectCast.GetCollider() == Global.Instance.player) {
             goTo = Global.Instance.player.GlobalPosition;
+            LookAt(Global.Instance.player.GlobalPosition);
         }
-
-        GlobalPosition = new Vector2(Mathf.Lerp(GlobalPosition.X, goTo.X, 0.01f), Mathf.Lerp(GlobalPosition.Y, goTo.Y, 0.01f));
+        GlobalPosition = new Vector2(Mathf.Lerp(GlobalPosition.X, goTo.X, speed), Mathf.Lerp(GlobalPosition.Y, goTo.Y, speed));
+        if (GlobalPosition.Round() == goTo.Round()) {
+            timer += delta;
+        }
+        if (timer > timeToGoBack) {
+            goTo = ogPos;
+            timer = 0;
+            LookAt(ogPos);
+        }
         
     }
 }
